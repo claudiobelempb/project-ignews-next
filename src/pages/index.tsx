2f5f135bccab692ff995d82d28af61f605fe34c7
claudiobelempb <claudio.c.lima@hotmail.com>
2021-07-06 11:34:26 -0300

    Title dinâmico por página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import Head from "next/head";
 import style from "../assets/styles/home.module.scss";
 
 export default function Home() {
-  return <div className={style.title}>Hello World!</div>;
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <div className={style.title}>Hello World!</div>
+    </>
+  );
 }
